02511eba5358f6097d98bed808a5b033bd1919db
AmeerSafadi-95 <ameersafadi95@gmail.com>
2020-08-02 20:17:28 +0300

    Updates

---
+++ precache-manifest.239f2a64306ae3bf15070062c9bbbb70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8edff8930d131f6ed1195e526f1537eb",
+    "url": "/BankApp/index.html"
+  },
+  {
+    "revision": "831eff28027c10d85235",
+    "url": "/BankApp/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "ab35548434834ebc02f8",
+    "url": "/BankApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "831eff28027c10d85235",
+    "url": "/BankApp/static/js/2.398bab49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BankApp/static/js/2.398bab49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab35548434834ebc02f8",
+    "url": "/BankApp/static/js/main.353352a0.chunk.js"
+  },
+  {
+    "revision": "4ab8164218abca80ec81",
+    "url": "/BankApp/static/js/runtime-main.f896ed7d.js"
+  }
+]);
